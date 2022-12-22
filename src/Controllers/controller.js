4b8aa6af0203db5bfe5a5a1bb1c56d99e6d78d74
Mahmut Can Kurt <mahmutcankurt@Mahmuts-MacBook-Pro.local>
2022-12-22 13:29:57 +0300

    bulkwriter added

--- src/Controllers/controller.js
+++ src/Controllers/controller.js
@@ -4,6 +4,7 @@ const service = require('../Services/service');
 
 const pokedex = db.collection('pokedex');
 let bulkWriter = db.bulkWriter()
+let writeBatch = db.batch()
 
 
 exports.createPokedex = async (req, res) => {
@@ -14,17 +15,48 @@ exports.createPokedex = async (req, res) => {
 
             const pokeRef = pokedex.doc(pokemon.id + '')
 
-            bulkWriter
+            await bulkWriter
                 .delete(pokeRef)
                 .then(
-                result => {
-                    console.log('Deleted successfully!');
+                    result => {
+                        console.log('Delete success!');
+                    })
+                .catch(err => {
+                    console.log('Deletion failed cause of: ', err);
+                });
+
+            await bulkWriter
+                .set(pokeRef, {pokemon})
+                .then(result => {
+                    console.log('BulkWriter success');
                 })
                 .catch(err => {
-                console.log('Deletion failed cause of: ', err);
-            });
+                    console.log('BulkWriter failed: ', err);
+                })
 
-            await pokedex.add(pokemon);
+
+
+            // setDoc(pokeRef, pokemon, {merge: true})
+            //     .then(pokeRef => {
+            //         console.log('Document has been added successfully!');
+            //     })
+            //     .catch(err => {
+            //         console.log('Adding failed cause of: ', err);
+            //     })
+
+            // await db.runTransaction(async (t) => {
+            //     const doc = await t.get(pokeRef);
+            //     const newPokemon = doc.data()
+            //     t.update(pokeRef, {pokemon: newPokemon})
+            // }).catch(err => {
+            //     console.log(err);
+            // })
+
+
+            // await batch.update(pokeRef, {pokedex : pokemon});
+
+
+            // await pokedex.add(pokemon);
 
         });
 
@@ -34,7 +66,9 @@ exports.createPokedex = async (req, res) => {
 
     } catch (error) {
 
-        return error.message;
+        const message = 'Error here: ' + error.message
+
+        return message;
 
     }
 
